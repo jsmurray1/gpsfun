38a319df674a60a1391a68bab446ebeb2227c8f8
Vincent Davis <vincent@heteroskedastic.com>
2020-08-23 08:06:13 -0600

    start of some new work, improve some exceptions, remove zwift files

--- gpsfun/gpsfun.py
+++ gpsfun/gpsfun.py
@@ -1,1 +1,26 @@
-"""Main module."""
+"""Mostly fuctions used elsewere"""
+
+def increase_points(df, freq='250L', slice=(None,None), columns=None):
+    """
+    TODO WORK IN PROGRESS
+    Need to be carefull and only include columns that it is meaningfull to interpilate between
+    freq: 250L is 250 milliseconds
+    Columns: you need to be sure the columns are not cumulative or is some other way derived from other when interpilating.
+    :param df:
+    :return:
+    """
+    if columns == None:
+        columns = ['Date_Time', 'Latitude', 'Longitude', 'Altitude']
+    df1 = df[columns][100:200].copy()
+    # TODO if we are only using Date_Time as the index then this is not needed.
+    df1.set_index(pd.DatetimeIndex(df1['Date_Time']), drop=True, inplace=True)
+    df1.drop('Date_Time', axis=1, inplace=True)
+    # df1.reset_index(drop=True, inplace=True)
+    print(df1[9:13].head(25))
+
+    ups = df1[10:20].resample('250L').asfreq().interpolate(method='linear', limit_direction='forward', axis=0)
+    df1 = df1.append(ups)
+    df1.sort_index(inplace=True)
+    # df1.reset_index(drop=True, inplace=True)
+
+    print(df1[9:20].head(25))
--- gpsfun/rallystyle.py
+++ gpsfun/rallystyle.py
@@ -1,5 +1,6 @@
 import numpy as np
 from datetime import timedelta
+
 try:
     from .exceptions import RallyStyleException, RallyResultsException, MatchCheckpointsException, CalcResultsException
 except:
@@ -35,13 +36,14 @@ class RallyResults(object):
     """
 
     def __init__(self, df, segments):
-        if {'Latitude', 'Longitude', 'Date_Time'}.intersection(set(df.columns)) != {'Latitude', 'Longitude', 'Date_Time'}:
+        if {'Latitude', 'Longitude', 'Date_Time'}.intersection(set(df.columns)) != {'Latitude', 'Longitude',
+                                                                                    'Date_Time'}:
             raise RallyStyleException(f"Dataframe must contains columns: \n {df.columns}")
         self.df = df
         self.init_columns = df.columns
         self.segments = segments
         self.epsilon = 0.00001  # used to for finding acute triangles
-        self.near = .0002 # How near the point needs to be to the checkpoint.
+        self.near = .0002  # How near the point needs to be to the checkpoint.
         self.results = []
         # self.ck_points = pd.DataFrame([p['location'] for p in segments], columns=['Latitude', 'Longitude'])
         self.ck_points = [p['location'] for p in segments]
@@ -52,6 +54,7 @@ class RallyResults(object):
         if not 'dist_to_next' in self.df.columns:
             self.df['dist_to_next'] = np.linalg.norm(self.df[['Latitude', 'Longitude']].values -
                                                      self.df[['shift_Latitude', 'shift_Longitude']].values, axis=1)
+
     def check_bounds(self):
         latmax = min([ck['lat'] for ck in self.ck_points]) >= self.df.Latitude.min()
         lonmax = min([ck['lon'] for ck in self.ck_points]) >= self.df.Longitude.min()
@@ -59,13 +62,35 @@ class RallyResults(object):
         lonmin = max([ck['lon'] for ck in self.ck_points]) <= self.df.Longitude.max()
         assert latmax and lonmax and latmin and lonmin, "This activity does not seem to be within the area of the event segments"
 
+    def select_near_points(self, check_point):
+        """
+        TODO: Work in progress
+        Selects points near the checkpoints:
+        These may be anywhere in the activity, but that seems ok.
+        """
+        df1['Date_Time'] = df1.Date_Time.astype(np.int64)
+        columns = ['Date_Time', 'Latitude', 'Longitude', 'Altitude']
+        start = 10
+        end = 11
+        rows = 7  # actualy get rows - 2
+        realend = (end - start) * 5 + start
+        for i in range(start, realend, rows):
+            curr_row = df1[columns].iloc[i]
+            next_row = df1[columns].iloc[i + 1]
+            new_df = pd.DataFrame(np.linspace(curr_row, next_row, rows), columns=columns)
+            df1 = pd.concat([df1[:i], new_df, df1[i + rows:]], ignore_index=True)
+
+        df1['Date_Time'] = df1.Date_Time.astype('datetime64[ns]')
+        print(df1[9:25].head(25))
+
+
+
     def match_checkpoints(self):
         """
         Identify the activity point the represents the arrival at the checkpoint
         find near points that form acute triangles
         """
         self.check_bounds()
-
         self.df['to_next'] = np.linalg.norm(self.df[['Latitude', 'Longitude']].values -
                                             self.df[['shift_Latitude', 'shift_Longitude']].values, axis=1)
         self.df['checkpoint'] = np.nan
@@ -79,14 +104,16 @@ class RallyResults(object):
                 self.df['acute'] = self.df[f'ck_to_A{i}'] ** 2 + self.df['to_next'] ** 2 <= self.df[
                     f'ck_to_B{i}'] ** 2 + self.epsilon
                 if self.df[f'ck_to_A{i}'].min() > self.near * 10:
-                    raise MatchCheckpointsException(f"It appears you never made it close to checkpoint {self.segments['segment_name']}")
+                    raise MatchCheckpointsException(
+                        f"It appears you never made it close to checkpoint {self.segments['segment_name']}")
                 self.df.loc[
                     self.df[row_slice:][(self.df[row_slice:][f'ck_to_A{i}'] <= self.near) &
                                         (self.df[row_slice:].acute)].index[0], ['checkpoint']] = i
                 row_slice = int(self.df[self.df.checkpoint == i].index[0])
                 self.df['seg_duration'] = self.df[self.df.checkpoint >= 0]['Date_Time'].diff()
             except Exception as e:
-                raise MatchCheckpointsException(f"Fail on checkpoint:{i} location: {ck}\nDataframe columns:\n{self.df.columns}")
+                raise MatchCheckpointsException(
+                    f"Fail on checkpoint:{i} location: {ck}\nDataframe columns:\n{self.df.columns}")
 
         self.df['seg_duration'] = self.df[self.df.checkpoint >= 0]['Date_Time'].diff()
         self.df['segment'] = self.df.checkpoint.fillna(method='ffill')
